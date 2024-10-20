65e55c604708005f4fe03963f92ae9ddcbe6ecc3
UpstreamData <75442874+UpstreamData@users.noreply.github.com>
2024-10-19 21:03:16 -0600

    Bug fixes for CORS.

--- bc/static/2024.js
+++ bc/static/2024.js
@@ -9,7 +9,9 @@ const parties = {
 }
 var regions = {'ABM': {'name': 'Abbotsford-Mission', 'parties': {}}, 'ABS': {'name': 'Abbotsford South', 'parties': {}}, 'ABW': {'name': 'Abbotsford West', 'parties': {}}, 'BDS': {'name': 'Boundary-Similkameen', 'parties': {}}, 'BLS': {'name': 'Bulkley Valley-Stikine', 'parties': {}}, 'BNC': {'name': 'Burnaby Centre', 'parties': {}}, 'BNE': {'name': 'Burnaby East', 'parties': {}}, 'BNN': {'name': 'Burnaby-New Westminster', 'parties': {}}, 'BNO': {'name': 'Burnaby North', 'parties': {}}, 'BNS': {'name': 'Burnaby South-Metrotown', 'parties': {}}, 'CBC': {'name': 'Cariboo-Chilcotin', 'parties': {}}, 'CHC': {'name': 'Chilliwack-Cultus Lake', 'parties': {}}, 'CHN': {'name': 'Chilliwack North', 'parties': {}}, 'CLR': {'name': 'Columbia River-Revelstoke', 'parties': {}}, 'CQB': {'name': 'Coquitlam-Burke Mountain', 'parties': {}}, 'CQM': {'name': 'Coquitlam-Maillardville', 'parties': {}}, 'CRC': {'name': 'Courtenay-Comox', 'parties': {}}, 'CWV': {'name': 'Cowichan Valley', 'parties': {}}, 'DLN': {'name': 'Delta North', 'parties': {}}, 'DLS': {'name': 'Delta South', 'parties': {}}, 'ESC': {'name': 'Esquimalt-Colwood', 'parties': {}}, 'FRN': {'name': 'Fraser-Nicola', 'parties': {}}, 'JFM': {'name': 'Juan de Fuca-Malahat', 'parties': {}}, 'KAC': {'name': 'Kamloops Centre', 'parties': {}}, 'KAN': {'name': 'Kamloops-North Thompson', 'parties': {}}, 'KEC': {'name': 'Kelowna Centre', 'parties': {}}, 'KLC': {'name': 'Kelowna-Lake Country-Coldstream', 'parties': {}}, 'KLM': {'name': 'Kelowna-Mission', 'parties': {}}, 'KOC': {'name': 'Kootenay Central', 'parties': {}}, 'KOM': {'name': 'Kootenay-Monashee', 'parties': {}}, 'KOR': {'name': 'Kootenay-Rockies', 'parties': {}}, 'LAO': {'name': 'Ladysmith-Oceanside', 'parties': {}}, 'LFH': {'name': 'Langford-Highlands', 'parties': {}}, 'LLA': {'name': 'Langley-Abbotsford', 'parties': {}}, 'LWG': {'name': 'Langley-Walnut Grove', 'parties': {}}, 'LWI': {'name': 'Langley-Willowbrook', 'parties': {}}, 'MAE': {'name': 'Maple Ridge East', 'parties': {}}, 'MAP': {'name': 'Maple Ridge-Pitt Meadows', 'parties': {}}, 'MPR': {'name': 'Mid Island-Pacific Rim', 'parties': {}}, 'NAI': {'name': 'Nanaimo-Gabriola Island', 'parties': {}}, 'NAL': {'name': 'Nanaimo-Lantzville', 'parties': {}}, 'NEC': {'name': 'Nechako Lakes', 'parties': {}}, 'NMC': {'name': 'New Westminster-Coquitlam', 'parties': {}}, 'NOH': {'name': 'North Coast-Haida Gwaii', 'parties': {}}, 'NOI': {'name': 'North Island', 'parties': {}}, 'NVL': {'name': 'North Vancouver-Lonsdale', 'parties': {}}, 'NVS': {'name': 'North Vancouver-Seymour', 'parties': {}}, 'OBG': {'name': 'Oak Bay-Gordon Head', 'parties': {}}, 'PCN': {'name': 'Peace River North', 'parties': {}}, 'PCS': {'name': 'Peace River South', 'parties': {}}, 'PES': {'name': 'Penticton-Summerland', 'parties': {}}, 'POC': {'name': 'Port Coquitlam', 'parties': {}}, 'POM': {'name': 'Port Moody-Burquitlam', 'parties': {}}, 'POR': {'name': 'Powell River-Sunshine Coast', 'parties': {}}, 'PRM': {'name': 'Prince George-Mackenzie', 'parties': {}}, 'PRO': {'name': 'Prince George-North Cariboo', 'parties': {}}, 'PRV': {'name': 'Prince George-Valemount', 'parties': {}}, 'RCB': {'name': 'Richmond-Bridgeport', 'parties': {}}, 'RCC': {'name': 'Richmond Centre', 'parties': {}}, 'RCQ': {'name': 'Richmond-Queensborough', 'parties': {}}, 'RCS': {'name': 'Richmond-Steveston', 'parties': {}}, 'SAN': {'name': 'Saanich North and the Islands', 'parties': {}}, 'SAS': {'name': 'Saanich South', 'parties': {}}, 'SHU': {'name': 'Salmon Arm-Shuswap', 'parties': {}}, 'SKE': {'name': 'Skeena', 'parties': {}}, 'SRC': {'name': 'Surrey City Centre', 'parties': {}}, 'SRD': {'name': 'Surrey-Cloverdale', 'parties': {}}, 'SRF': {'name': 'Surrey-Fleetwood', 'parties': {}}, 'SRG': {'name': 'Surrey-Guildford', 'parties': {}}, 'SRN': {'name': 'Surrey-Newton', 'parties': {}}, 'SUN': {'name': 'Surrey North', 'parties': {}}, 'SUP': {'name': 'Surrey-Panorama', 'parties': {}}, 'SUR': {'name': 'Surrey-Serpentine River', 'parties': {}}, 'SUS': {'name': 'Surrey South', 'parties': {}}, 'SWR': {'name': 'Surrey-White Rock', 'parties': {}}, 'VFV': {'name': 'Vancouver-Fraserview', 'parties': {}}, 'VHA': {'name': 'Vancouver-Hastings', 'parties': {}}, 'VKE': {'name': 'Vancouver-Kensington', 'parties': {}}, 'VLA': {'name': 'Vancouver-Langara', 'parties': {}}, 'VLM': {'name': 'Vancouver-Little Mountain', 'parties': {}}, 'VNP': {'name': 'Vancouver-Point Grey', 'parties': {}}, 'VNQ': {'name': 'Vancouver-Quilchena', 'parties': {}}, 'VNR': {'name': 'Vancouver-Renfrew', 'parties': {}}, 'VNS': {'name': 'Vancouver-South Granville', 'parties': {}}, 'VNT': {'name': 'Vancouver-Strathcona', 'parties': {}}, 'VNW': {'name': 'Vancouver-West End', 'parties': {}}, 'VNY': {'name': 'Vancouver-Yaletown', 'parties': {}}, 'VRL': {'name': 'Vernon-Lumby', 'parties': {}}, 'VTB': {'name': 'Victoria-Beacon Hill', 'parties': {}}, 'VTS': {'name': 'Victoria-Swan Lake', 'parties': {}}, 'WKP': {'name': 'West Kelowna-Peachland', 'parties': {}}, 'WVC': {'name': 'West Vancouver-Capilano', 'parties': {}}, 'WVS': {'name': 'West Vancouver-Sea to Sky', 'parties': {}}}
 
-const electoralCsvUrl = 'https://electionsbcenr.blob.core.windows.net/electionsbcenr/GE-2024-10-19_Candidate.csv';
+//const electoralCsvUrl = 'https://electionsbcenr.blob.core.windows.net/electionsbcenr/GE-2024-10-19_Candidate.csv';
+
+const electoralCsvUrl = 'https://corsproxy.io/?' + encodeURIComponent('https://electionsbcenr.blob.core.windows.net/electionsbcenr/GE-2024-10-19_Candidate.csv');
 
 let locations = [];
 document.addEventListener("DOMContentLoaded", () => {
@@ -72,12 +74,7 @@ function updateCounts() {
 }
 async function fetchElectoralData(url) {
     try {
-        const response = await fetch(url, {mode: "no-cors"});
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
+        const response = await fetch(url);
         const csvData = await response.text();
         const parsedData = parseElectoralCSV(csvData);
         const winningParties = getTopPartyByDistrict(parsedData);
@@ -124,7 +121,11 @@ function getTopPartyByDistrict(parsedData) {
         const voteCount = parseInt(votes, 10); // Convert votes to a number
         const abbreviatedParty = partyMapping[party] || 'TBD';
         if (!topParties[district] || voteCount > topParties[district].votes) {
-            topParties[district] = { party: abbreviatedParty, votes: voteCount };
+            if (!(voteCount == 0)) {
+                topParties[district] = { party: abbreviatedParty, votes: voteCount };
+            } else {
+                topParties[district] = { party: "TBD", votes: 0 };
+            }
         }
     });
 
